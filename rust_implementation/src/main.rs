de2ecf2ed0750fb8ca7339a0d78490c6cddac6cf
AnotherDev <joseluismanco37@gmail.com>
2024-08-31 05:03:15 -0300

    implement s-expressions in rust

--- rust_implementation/src/main.rs
+++ rust_implementation/src/main.rs
@@ -14,20 +14,29 @@ struct AnotlispParser;
 enum LvalType {
     LvalNum,
     LvalErr,
+    LvalSym,
+    LvalSexpr,
 }
 
-#[derive(Debug, Clone, Copy)]
-enum LvalErr {
-    DivZero,
-    BadOp,
-    BadNum,
-}
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 struct Lval {
     lval_type: LvalType,
     num: Option<i128>,
-    err: Option<LvalErr>,
+    err: Option<String>,
+    sym: Option<String>,
+    cell: Vec<Lval>,
+}
+
+impl Default for Lval {
+    fn default() -> Self {
+        Lval {
+            lval_type: LvalType::LvalNum,
+            num: None,
+            err: None,
+            sym: None,
+            cell: Vec::new(),
+        }
+    }
 }
 
 impl Lval {
@@ -35,84 +44,220 @@ impl Lval {
         Lval {
             lval_type: LvalType::LvalNum,
             num: Some(value),
-            err: None,
+            ..Default::default()
         }
     }
 
-    fn new_err(err: LvalErr) -> Self {
+    fn new_err(err: String) -> Self {
         Lval {
             lval_type: LvalType::LvalErr,
-            num: None,
             err: Some(err),
+            ..Default::default()
         }
     }
-}
 
+    fn new_sym(sym: String) -> Self {
+        Lval {
+            lval_type: LvalType::LvalSym,
+            sym: Some(sym),
+            ..Default::default()
+        }
+    }
+
+    fn new_sexpr() -> Self {
+        Lval {
+            lval_type: LvalType::LvalSexpr,
+            ..Default::default()
+        }
+    }
+
+    fn add(&mut self, value: Lval) -> &mut Self {
+        self.cell.push(value);
+        return self;
+    }
+
+    fn pop(&mut self, index: usize) -> Lval {
+        return self.cell.remove(index);
+    }
+
+    fn builtin_op(&mut self, op: &String) -> Lval {
+        for lval in &self.cell {
+            if lval.lval_type != LvalType::LvalNum {
+                return Lval::new_err(String::from("Cannot operate on non-number!"));
+            }
+        }
+
+        let mut x = self.pop(0);
+
+        if op == "-" && self.cell.is_empty() {
+            match x.num {
+                Some(ref mut num) => *num = -*num,
+                None => return Lval::new_err(String::from("Cannot negate non-number!")),
+            }
+        }
+
+        while !self.cell.is_empty() {
+            let y = self.pop(0);
+            if let (Some(ref mut x_num), Some(y_num)) = (x.num, y.num) {
+                match op.as_str() {
+                    "+" => *x_num += y_num,
+                    "-" => *x_num -= y_num,
+                    "*" => *x_num *= y_num,
+                    "/" => {
+                        if y_num == 0 {
+                            return Lval::new_err("Division by zero!".to_string());
+                        }
+                        *x_num /= y_num;
+                    }
+                    "%" => {
+                        if y_num == 0 {
+                            return Lval::new_err("Division by zero!".to_string());
+                        }
+                        *x_num %= y_num;
+                    }
+                    _ => return Lval::new_err("Invalid operator!".to_string()),
+                }
+            } else {
+                return Lval::new_err("Invalid number!".to_string());
+            }
+        }
+
+        x
+    }
+    fn eval(&mut self) -> Lval {
+        if self.lval_type == LvalType::LvalSexpr {
+            return self.eval_sexpr();
+        }
+        self.clone()
+    }
+    fn eval_sexpr(&mut self) -> Lval {
+        for lval in &mut self.cell {
+            lval.eval();
+        }
+
+        for lval in &self.cell {
+            if lval.lval_type == LvalType::LvalErr {
+                return lval.clone();
+            }
+        }
+
+        if self.cell.is_empty() || self.cell.len() == 1 {
+            return self.clone();
+        }
+
+        let f = self.pop(0);
+
+        if f.lval_type != LvalType::LvalSym {
+            return Lval::new_err(String::from("S-Expression does not start with symbol!"));
+        }
+
+        self.builtin_op(f.sym.as_ref().unwrap())
+    }
+
+    fn read_num(pair: Pair<Rule>) -> Lval {
+        let num = pair.as_str().parse::<i128>();
+        match num {
+            Ok(num) => Lval::new_num(num),
+            Err(_) => Lval::new_err(String::from("Invalid number!")),
+        }
+    }
+    fn read(mut pairs: Pairs<Rule>) -> Lval {
+        let mut x;
+        if pairs.clone().next().is_none() {
+            return Lval::new_err(String::from("Empty expression!"));
+        }
+        let first_pair = pairs.next().unwrap();
+        match first_pair.as_rule() {
+            Rule::number => return Lval::read_num(first_pair),
+            Rule::symbol => return Lval::new_sym(first_pair.as_str().to_string()),
+            Rule::sexpression => x = Lval::new_sexpr(),
+            _ => return Lval::new_err(String::from("Unknown expression!")),
+        };
+
+        for inner_pair in pairs {
+            match inner_pair.as_rule() {
+                Rule::sexpression | Rule::expression => x.add(Lval::read(inner_pair.into_inner())),
+                Rule::symbol => x.add(Lval::new_sym(inner_pair.as_str().to_string())),
+                Rule::number => x.add(Lval::read_num(inner_pair)),
+                _ => continue,
+            };
+        }
+
+        x
+    }
+}
 impl fmt::Display for Lval {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self.lval_type {
             LvalType::LvalNum => write!(f, "{}", self.num.unwrap()),
-            LvalType::LvalErr => match self.err.unwrap() {
-                LvalErr::DivZero => write!(f, "Error: Division by zero"),
-                LvalErr::BadOp => write!(f, "Error: Invalid operator"),
-                LvalErr::BadNum => write!(f, "Error: Invalid number"),
-            },
-        }
-    }
-}
-
-fn eval_op(x: Lval, op: char, y: Lval) -> Lval {
-    if x.lval_type == LvalType::LvalErr {
-        return x;
-    }
-    if y.lval_type == LvalType::LvalErr {
-        return y;
-    }
-    match op {
-        '+' => Lval::new_num(x.num.unwrap() + y.num.unwrap()),
-        '-' => Lval::new_num(x.num.unwrap() - y.num.unwrap()),
-        '*' => Lval::new_num(x.num.unwrap() * y.num.unwrap()),
-        '/' => {
-            if y.num.unwrap() == 0 {
-                Lval::new_err(LvalErr::DivZero)
-            } else {
-                Lval::new_num(x.num.unwrap() / y.num.unwrap())
+            LvalType::LvalSym => write!(f, "{}", self.sym.as_ref().unwrap()),
+            LvalType::LvalSexpr => {
+                write!(f, "(")?;
+                for (i, lval) in self.cell.iter().enumerate() {
+                    if i > 0 {
+                        write!(f, " ")?;
+                    }
+                    write!(f, "{}", lval)?;
+                }
+                write!(f, ")")
             }
+            LvalType::LvalErr => write!(f, "Error: {}", self.err.as_ref().unwrap()),
         }
-        '%' => {
-            if y.num.unwrap() == 0 {
-                Lval::new_err(LvalErr::DivZero)
-            } else {
-                Lval::new_num(x.num.unwrap() % y.num.unwrap())
-            }
-        }
-        _ => Lval::new_err(LvalErr::BadOp),
     }
 }
 
-fn eval(pairs: &Pairs<Rule>) -> Lval {
-    let mut pairs = pairs.clone();
-    let first_pair = pairs.next().unwrap();
+// fn eval_op(x: Lval, op: char, y: Lval) -> Lval {
+//     if x.lval_type == LvalType::LvalErr {
+//         return x;
+//     }
+//     if y.lval_type == LvalType::LvalErr {
+//         return y;
+//     }
+//     match op {
+//         '+' => Lval::new_num(x.num.unwrap() + y.num.unwrap()),
+//         '-' => Lval::new_num(x.num.unwrap() - y.num.unwrap()),
+//         '*' => Lval::new_num(x.num.unwrap() * y.num.unwrap()),
+//         '/' => {
+//             if y.num.unwrap() == 0 {
+//                 Lval::new_err(LvalErr::DivZero)
+//             } else {
+//                 Lval::new_num(x.num.unwrap() / y.num.unwrap())
+//             }
+//         }
+//         '%' => {
+//             if y.num.unwrap() == 0 {
+//                 Lval::new_err(LvalErr::DivZero)
+//             } else {
+//                 Lval::new_num(x.num.unwrap() % y.num.unwrap())
+//             }
+//         }
+//         _ => Lval::new_err(LvalErr::BadOp),
+//     }
+// }
 
-    if first_pair.as_rule() == Rule::number {
-        let number = first_pair.as_str().parse::<i128>();
-        match number {
-            Ok(num) => return Lval::new_num(num),
-            Err(_) => return Lval::new_err(LvalErr::BadNum),
-        }
-    }
-
-    let op = &first_pair.as_str().chars().next().unwrap();
-    let mut x = eval(&pairs.next().unwrap().into_inner());
-    while let Some(next_pair) = pairs.next() {
-        if next_pair.as_rule() == Rule::expression {
-            x = eval_op(x, *op, eval(&next_pair.into_inner()));
-        } else {
-            break;
-        }
-    }
-    return x;
-}
+// fn eval(pairs: &Pairs<Rule>) -> Lval {
+//     let mut pairs = pairs.clone();
+//     let first_pair = pairs.next().unwrap();
+//
+//     if first_pair.as_rule() == Rule::number {
+//         let number = first_pair.as_str().parse::<i128>();
+//         match number {
+//             Ok(num) => return Lval::new_num(num),
+//             Err(_) => return Lval::new_err(LvalErr::BadNum),
+//         }
+//     }
+//
+//     let op = &first_pair.as_str().chars().next().unwrap();
+//     let mut x = eval(&pairs.next().unwrap().into_inner());
+//     while let Some(next_pair) = pairs.next() {
+//         if next_pair.as_rule() == Rule::expression {
+//             x = eval_op(x, *op, eval(&next_pair.into_inner()));
+//         } else {
+//             break;
+//         }
+//     }
+//     return x;
+// }
 
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
@@ -129,7 +274,8 @@ fn main() -> io::Result<()> {
 
         match parse_result {
             Ok(parsed) => {
-                let result = eval(&parsed.clone().next().unwrap().into_inner());
+                let result =
+                    Lval::eval(&mut Lval::read(parsed.clone().next().unwrap().into_inner()));
 
                 println!("Result: {}", result);
             }
@@ -140,15 +286,4 @@ fn main() -> io::Result<()> {
 
         buffer.clear();
     }
-}
-fn format_pair(pair: &Pair<Rule>, indent: usize) -> String {
-    let mut formatted = String::new();
-    formatted.push_str(&" ".repeat(indent));
-    formatted.push_str(&format!("{:?}: \"{}\"\n", pair.as_rule(), pair.as_str()));
-
-    for inner_pair in pair.clone().into_inner() {
-        formatted.push_str(&format_pair(&inner_pair, indent + 2));
-    }
-
-    formatted
 }
